634dcd354d24c42bd46819b68bcca176c05382db
Michael B <michael@ramyun.co.uk>
2024-10-13 18:44:04 +0200

    Papaya!

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,10 +2,18 @@ return {
   "goolord/alpha-nvim",
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local startify = require("alpha.themes.startify")
-    startify.file_icons.provider = "devicons"
-    require("alpha").setup(
-      startify.config
-    )
+    local dashboard = require("alpha.themes.startify")
+    local header = {
+      "┏┓              ",
+      "┃┃┏┓┏┓┏┓┓┏┏┓    ",
+      "┣┛┗┻┣┛┗┻┗┫┗┻ Vim",
+      "    ┃    ┛      ",
+    }
+    local header_color = "Identifier" -- This is yellow in Kanagawa
+
+    dashboard.file_icons.provider = "devicons"
+    dashboard.section.header.opts.hl = header_color
+    dashboard.section.header.val = header
+    require("alpha").setup(dashboard.config)
   end,
 }
