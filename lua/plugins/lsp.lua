ca69a9543c9331c7a6aab6ed135d351759f7f2ad
Michael B <michael@ramyun.co.uk>
2024-10-13 18:19:25 +0200

    Add rust analyzer and add diagnostics keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,10 +22,14 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.rust_analyzer.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostic" })
       vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Show code actions" })
+      vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+      vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
       vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename symbol" })
     end,
     dependencies = { "williamboman/mason-lspconfig.nvim" }
