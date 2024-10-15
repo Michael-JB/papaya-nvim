27fc529f39739537c4f10b3f48567f600fcfeede
Michael Barlow <michael.barlow@aleph-alpha.com>
2024-10-15 14:57:36 +0200

    Add helm and yaml LSPs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,13 @@ return {
     opts = {},
   },
   {
+    -- Helm sucks: it requires that yaml templates have a `.yaml` extension, even though they're
+    -- not valid yaml files. Because of this, we summon `vim-helm` which adds some vim-script to
+    -- correctly classify these files as `helm` filetype to prevent the yaml LS attaching. Oh Helm.
+    "towolf/vim-helm",
+    ft = "helm",
+  },
+  {
     "neovim/nvim-lspconfig",
     event = "VeryLazy",
     dependencies = {
@@ -19,6 +26,8 @@ return {
       -- Configure language servers here
       lspconfig.lua_ls.setup({})
       lspconfig.rust_analyzer.setup({})
+      lspconfig.yamlls.setup({})
+      lspconfig.helm_ls.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
@@ -27,6 +36,7 @@ return {
       vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
       vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
       vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set("n", "<leader>li", ":LspInfo<CR>", { desc = "Show LSP information" })
     end,
   },
 }
