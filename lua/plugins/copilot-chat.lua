d10f735b68d6d0114a4f339f6ae30ec11bef0004
Michael Barlow <michael.barlow@aleph-alpha.com>
2024-10-16 14:57:30 +0200

    Load copilot on start

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,37 +1,43 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  branch = "canary",
-  dependencies = {
+  {
     "github/copilot.vim",
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
+    event = "VeryLazy",
   },
-  keys = {
-    { "<leader>ac", "<cmd>CopilotChatToggle <CR>", mode = { "n", "v" }, desc = "Toggle copilot chat" },
-    { "<leader>ae", "<cmd>CopilotChatExplain <CR>", mode = { "n", "v" }, desc = "Explain selection" },
-    { "<leader>ar", "<cmd>CopilotChatReview <CR>", mode = { "n", "v" }, desc = "Review selection" },
-    { "<leader>ad", "<cmd>CopilotChatDocs <CR>", mode = { "n", "v" }, desc = "Document selection" },
-    { "<leader>at", "<cmd>CopilotChatTests <CR>", mode = { "n", "v" }, desc = "Test selection" },
-    { "<leader>af", "<cmd>CopilotChatFixDiagnostic <CR>", mode = { "n", "v" }, desc = "Fix diagnostic" },
-    { "<leader>ao", "<cmd>CopilotChatOptimize <CR>", mode = { "n", "v" }, desc = "Optimize selection" },
-  },
-  config = function()
-    require("CopilotChat").setup({
-      show_help = false,
-      window = {
-        layout = "float",
-        width = 0.7,
-        height = 0.7,
-        border = "rounded",
-      },
-      mappings = {
-        complete = {
-          -- This clashes with github/copilot.vim tab completion so disable it
-          insert = "",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      "github/copilot.vim",
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    keys = {
+      { "<leader>ac", "<cmd>CopilotChatToggle <CR>", mode = { "n", "v" }, desc = "Toggle copilot chat" },
+      { "<leader>ae", "<cmd>CopilotChatExplain <CR>", mode = { "n", "v" }, desc = "Explain selection" },
+      { "<leader>ar", "<cmd>CopilotChatReview <CR>", mode = { "n", "v" }, desc = "Review selection" },
+      { "<leader>ad", "<cmd>CopilotChatDocs <CR>", mode = { "n", "v" }, desc = "Document selection" },
+      { "<leader>at", "<cmd>CopilotChatTests <CR>", mode = { "n", "v" }, desc = "Test selection" },
+      { "<leader>af", "<cmd>CopilotChatFixDiagnostic <CR>", mode = { "n", "v" }, desc = "Fix diagnostic" },
+      { "<leader>ao", "<cmd>CopilotChatOptimize <CR>", mode = { "n", "v" }, desc = "Optimize selection" },
+    },
+    config = function()
+      require("CopilotChat").setup({
+        show_help = false,
+        window = {
+          layout = "float",
+          width = 0.7,
+          height = 0.7,
+          border = "rounded",
         },
-      },
-    })
+        mappings = {
+          complete = {
+            -- This clashes with github/copilot.vim tab completion so disable it
+            insert = "",
+          },
+        },
+      })
 
-    require("CopilotChat.integrations.cmp").setup()
-  end,
+      require("CopilotChat.integrations.cmp").setup()
+    end,
+  },
 }
