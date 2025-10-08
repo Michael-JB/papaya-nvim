fb6d48bf0563b843fc5ccd8be5a867d9978e5412
Michael Barlow <michael@ramyun.co.uk>
2025-10-08 21:00:47 +0200

    fix formatting

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,10 +5,10 @@ return {
     current_line_blame = true,
     current_line_blame_opts = {
       delay = 400,
-    }
+    },
   },
   keys = {
     { "<leader>gl", ":Gitsigns blame_line<CR>", desc = "Open line blame" },
     { "<leader>gb", ":Gitsigns blame<CR>", desc = "Open buffer blame" },
-  }
+  },
 }
